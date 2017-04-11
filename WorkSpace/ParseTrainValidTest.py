00a0593d72cf4633e61f9824dedd405b7097db8f
EliQinZZ <EliQinZZ1997@gmail.com>
2017-04-11 00:02:46 -0700

    still need running

--- WorkSpace/ParseTrainValidTest.py
+++ WorkSpace/ParseTrainValidTest.py
@@ -31,10 +31,10 @@ train_size = int(train_percent * total_workout)
 validation_size = int(validation_percent * total_workout)
 test_size = total_workout - train_size - validation_size
 
-print("Size of total workout for top 8 is {:d}".format(total_workout))
-print("Size of training set for top 8 is {:d}".format(train_size))
-print("Size of validation set for top 8 is {:d}".format(validation_size))
-print("Size of test set for top 8 is {:d}\n".format(test_size))
+print("Size of total workout for top 4 is {:d}".format(total_workout))
+print("Size of training set for top 4 is {:d}".format(train_size))
+print("Size of validation set for top 4 is {:d}".format(validation_size))
+print("Size of test set for top 4 is {:d}\n".format(test_size))
 
 # Split the workout data according to the calculated sizes above
 # Note: slicing ":" is left-inclusive and right-exclusive
@@ -43,21 +43,21 @@ validation_list = workout_list[train_size: train_size + validation_size]
 test_list = workout_list[train_size + validation_size:]
 
 # Write into separate files
-tr_out = gzip.open("../TrainValidTest/training_set_top_8.json.gz", 'wb')
-va_out = gzip.open("../TrainValidTest/validation_set_top_8.json.gz", 'wb')
-te_out = gzip.open("../TrainValidTest/test_set_top_8.json.gz", 'wb')
+tr_out = gzip.open("../TrainValidTest/training_set_top_4.json.gz", 'wb')
+va_out = gzip.open("../TrainValidTest/validation_set_top_4.json.gz", 'wb')
+te_out = gzip.open("../TrainValidTest/test_set_top_4.json.gz", 'wb')
 
 for workout in train_list:
     tr_out.write(workout)
-print("Finished writing training set for top 8")
+print("Finished writing training set for top 4")
 
 for workout in validation_list:
     va_out.write(workout)
-print("Finished writing validation set for top 8")
+print("Finished writing validation set for top 4")
 
 for workout in test_list:
     te_out.write(workout)
-print("Finished writing test set for top 8")
+print("Finished writing test set for top 4")
 
 tr_out.close()
 va_out.close()
